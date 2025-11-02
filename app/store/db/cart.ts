ee325bd2d493e9319c80c4558d8e175c4cbc7313
Radek Kyselý <radek@kysely.org>
2025-11-02 02:42:49 +0100

    feat: Expire Cart by various time based on status

--- app/store/db/cart.ts
+++ app/store/db/cart.ts
@@ -3,6 +3,13 @@ import { Cart } from "@/app/store/objects/cart";
 
 const storageKey = (cartId: string) => `cart:${cartId}`;
 
+const EXPIRE_AFTER_SECONDS: Record<Cart["status"], number> = {
+  shopping: 60 * 60 * 1, // 1 hour
+  checkout: 60 * 60 * 1, // 1 hour
+  completed: 60 * 60 * 24, // 24 hours
+  cancelled: 60 * 60 * 1, // 1 hour
+};
+
 export const loadCart = async (cartId: string): Promise<Cart | null> => {
   try {
     const cart = await db.get<Cart>(storageKey(cartId));
@@ -18,7 +25,7 @@ export const saveCart = async (cart: Cart): Promise<void> => {
     await db
       .multi()
       .set(storageKey(cart.id), JSON.stringify(cart))
-      .expire(storageKey(cart.id), 60 * 60 * 24) // 24 hours in seconds
+      .expire(storageKey(cart.id), EXPIRE_AFTER_SECONDS[cart.status])
       .exec();
   } catch (error) {
     console.error(`Failed to save cart ${cart.id}:`, error);
